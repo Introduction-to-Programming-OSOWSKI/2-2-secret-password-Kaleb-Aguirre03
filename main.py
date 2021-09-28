04efe15746269e4479e83bfaac6dab2fca743284
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-28 09:25:15 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+#Define the Function password() with if and else statements
+def password(p):
+    if p == "knights19":
+        return "ACCESS GRANTED"
+
+    else:
+        return "ACCESS DENIED"
+
+#Print the function with an incorrect password
+print (password("sakljdf;kgj;jfb"))
+
+#Print the function with the correct password
+print (password("knights19"))
